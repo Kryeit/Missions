39052c3c194e5ca83a646a3ff3c260d501e79904
muriplz <46825658+muriplz@users.noreply.github.com>
2023-05-12 22:02:56 +0200

    first commit - Project setup

---
+++ fabric/src/main/java/com/kryeit/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package com.kryeit.fabric;
+
+import com.kryeit.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
