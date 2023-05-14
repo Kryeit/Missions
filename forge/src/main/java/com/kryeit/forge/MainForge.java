8bb7bb6f908f7402fa58d39f81f1e396723c8160
muriplz <46825658+muriplz@users.noreply.github.com>
2023-05-14 16:10:51 +0200

    Forge key bind

--- forge/src/main/java/com/kryeit/forge/MainForge.java
+++ forge/src/main/java/com/kryeit/forge/MainForge.java
@@ -1,13 +1,22 @@
 package com.kryeit.forge;
 
 import com.kryeit.Main;
+import com.kryeit.forge.client.KeyInit;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Main.MOD_ID)
 public class MainForge {
+
     public MainForge() {
-        // Submit our event bus to let architectury register our content on the right time
         Main.init();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        KeyInit.init();
+    }
+
+
 }
---
+++ forge/src/main/java/com/kryeit/forge/client/KeyInit.java
@@ -0,0 +1,24 @@
+package com.kryeit.forge.client;
+
+import com.kryeit.Main;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.ClientRegistry;
+
+public final class KeyInit {
+    public static KeyMapping missionGuiKey;
+
+    private KeyInit() {
+    }
+
+    public static void init() {
+        missionGuiKey = registerKey("mission_gui", KeyMapping.CATEGORY_GAMEPLAY, InputConstants.KEY_H);
+    }
+
+    private static KeyMapping registerKey(String name, String category, int keycode) {
+        final var key = new KeyMapping("key." + Main.MOD_ID + "." + name, keycode, category);
+        ClientRegistry.registerKeyBinding(key);
+        return key;
+    }
+}
+
