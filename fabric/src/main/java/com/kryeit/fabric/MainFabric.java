33a036bb2b843f3a3ffbd279497feec0f8b1f94c
muriplz <46825658+muriplz@users.noreply.github.com>
2023-05-23 20:40:15 +0200

    added the ResourceLocation

--- fabric/src/main/java/com/kryeit/fabric/MainFabric.java
+++ fabric/src/main/java/com/kryeit/fabric/MainFabric.java
@@ -1,9 +1,13 @@
 package com.kryeit.fabric;
 
 import com.kryeit.Main;
+import com.kryeit.fabric.packet.ActiveMissionProviderImpl;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
 
 public class MainFabric implements ModInitializer {
+    public static final ResourceLocation OPEN_GUI_PACKET = new ResourceLocation(Main.MOD_ID, "open_gui");
+
     @Override
     public void onInitialize() {
         Main.init();
