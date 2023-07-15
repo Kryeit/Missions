041a0907db0185755bef93266271d4f9b95dd4c9
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2023-07-15 12:38:07 +0200

    Make reassignment work again

--- forge/src/main/java/com/kryeit/forge/MainForge.java
+++ forge/src/main/java/com/kryeit/forge/MainForge.java
@@ -1,19 +1,18 @@
 package com.kryeit.forge;
 
 import com.kryeit.Main;
-import com.kryeit.forge.init.ModBlocks;
-import com.kryeit.forge.init.ModBlockEntities;
-import com.kryeit.forge.init.KeyInit;
-import com.kryeit.forge.init.ModItems;
-import com.kryeit.forge.init.ModRecipes;
-import com.kryeit.forge.init.ModMenuTypes;
+import com.kryeit.forge.init.*;
 import com.kryeit.forge.tab.CreativeModeTabs;
+import com.kryeit.missions.MissionManager;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.function.Consumer;
 
 @Mod(Main.MOD_ID)
 public class MainForge {
@@ -22,8 +21,6 @@ public class MainForge {
 
 
     public MainForge() {
-
-
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
@@ -39,13 +36,12 @@ public class MainForge {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         forgeEventBus.register(new MissionHandler());
-        //TODO: THIS DOESNT WORK, THE LISTENER IS WRONG ARGUMENT OR SOMETHING
-        //eventBus.addListener((Consumer<PlayerEvent.PlayerLoggedInEvent>) event -> {
-        //    boolean reassigned = MissionManager.reassignMissionsIfNecessary(event.getPlayer().getUUID());
-        //    if (reassigned) {
-        //        // send a message, I don't know?
-        //    }
-        //});
+        forgeEventBus.addListener((Consumer<PlayerEvent.PlayerLoggedInEvent>) event -> {
+            boolean reassigned = MissionManager.reassignMissionsIfNecessary(event.getPlayer().getUUID());
+            if (reassigned) {
+                // send a message, I don't know?
+            }
+        });
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
