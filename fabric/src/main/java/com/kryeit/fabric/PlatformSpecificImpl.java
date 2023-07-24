4a3335b0ac921643d9beacd4b65bb471754625a4
muriplz <46825658+muriplz@users.noreply.github.com>
2023-07-24 15:19:01 +0200

    getResourceLocation() is platform specific

--- fabric/src/main/java/com/kryeit/fabric/PlatformSpecificImpl.java
+++ fabric/src/main/java/com/kryeit/fabric/PlatformSpecificImpl.java
@@ -3,10 +3,17 @@ package com.kryeit.fabric;
 import com.kryeit.PlatformSpecific;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 
 public class PlatformSpecificImpl extends PlatformSpecific {
     public static boolean isClient() {
         // For Fabric
         return FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
+    }
+
+    public static ResourceLocation getResourceLocation(Item item) {
+        return Registry.ITEM.getKey(item);
     }
 }
--- forge/src/main/java/com/kryeit/forge/PlatformSpecificImpl.java
+++ forge/src/main/java/com/kryeit/forge/PlatformSpecificImpl.java
@@ -1,11 +1,19 @@
 package com.kryeit.forge;
 
 import com.kryeit.PlatformSpecific;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 public class PlatformSpecificImpl extends PlatformSpecific {
     public static boolean isClient() {
         // For Forge
         return FMLLoader.getDist().isClient();
+    }
+
+    public static ResourceLocation getResourceLocation(Item item) {
+        return item.getRegistryName();
     }
 }
