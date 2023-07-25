cfd55550cf7d4fefb513a4839456a407813b08a7
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2023-07-25 19:58:48 +0200

    Fix events being fired twice

--- forge/src/main/java/com/kryeit/forge/MissionHandler.java
+++ forge/src/main/java/com/kryeit/forge/MissionHandler.java
@@ -6,7 +6,8 @@ import com.kryeit.missions.mission_types.CraftMission;
 import com.kryeit.missions.mission_types.EatMission;
 import com.kryeit.missions.mission_types.KillMission;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -17,13 +18,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class MissionHandler {
     @SubscribeEvent
     public void breakBlock(BlockEvent.BreakEvent event) {
+        if (isNotServerPlayer(event.getPlayer())) return;
         ResourceLocation item = event.getState().getBlock().getRegistryName();
         MissionTypeRegistry.INSTANCE.getType(BreakMission.class).handleItem(event.getPlayer().getUUID(), item);
     }
 
     @SubscribeEvent
     public void eatItem(LivingEntityUseItemEvent.Finish event) {
-        if (event.getEntity() instanceof Player player) {
+        if (event.getEntity() instanceof ServerPlayer player) {
             ResourceLocation item = event.getItem().getItem().getRegistryName();
             MissionTypeRegistry.INSTANCE.getType(EatMission.class).handleItem(player.getUUID(), item);
         }
@@ -31,7 +33,7 @@ public class MissionHandler {
 
     @SubscribeEvent
     public void killEntity(LivingDeathEvent event) {
-        if (event.getSource().getEntity() instanceof Player player) {
+        if (event.getSource().getEntity() instanceof ServerPlayer player) {
             ResourceLocation entity = event.getEntity().getType().getRegistryName();
             MissionTypeRegistry.INSTANCE.getType(KillMission.class).handleItem(player.getUUID(), entity);
         }
@@ -39,6 +41,7 @@ public class MissionHandler {
 
     @SubscribeEvent
     public void craftItem(PlayerEvent.ItemCraftedEvent event) {
+        if (isNotServerPlayer(event.getEntity())) return;
         ResourceLocation item = event.getCrafting().getItem().getRegistryName();
         MissionTypeRegistry.INSTANCE.getType(CraftMission.class).handleItem(event.getPlayer().getUUID(), item);
     }
@@ -46,5 +49,9 @@ public class MissionHandler {
     @SubscribeEvent
     public void useItem(PlayerInteractEvent event) {
         event.getItemStack();
+    }
+
+    private boolean isNotServerPlayer(Entity entity) {
+        return !(entity instanceof ServerPlayer);
     }
 }
