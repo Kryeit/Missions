d479152e3f0c5bbf58378f5f8b5ab0590b802b1f
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2023-07-29 15:13:39 +0200

    FishMission should show normal items

--- common/src/main/java/com/kryeit/missions/mission_types/FishMission.java
+++ common/src/main/java/com/kryeit/missions/mission_types/FishMission.java
@@ -1,10 +1,7 @@
 package com.kryeit.missions.mission_types;
 
-import com.kryeit.Utils;
 import com.kryeit.missions.MissionDifficulty;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
 
 public class FishMission implements MultiResourceMissionType {
     @Override
@@ -20,10 +17,5 @@ public class FishMission implements MultiResourceMissionType {
     @Override
     public Component description() {
         return Component.nullToEmpty("Fishing mission");
-    }
-
-    @Override
-    public ItemStack getItemStack(ResourceLocation item) {
-        return Utils.getSpawnEggOfEntity(item);
     }
 }
