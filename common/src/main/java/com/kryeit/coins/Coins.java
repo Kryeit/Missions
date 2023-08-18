05f7d74923f4d340951ab15090ffe1a2ad45291a
muriplz <46825658+muriplz@users.noreply.github.com>
2023-08-18 18:12:50 +0200

    wait sorry this is needed

--- common/src/main/java/com/kryeit/coins/Coins.java
+++ common/src/main/java/com/kryeit/coins/Coins.java
@@ -12,8 +12,9 @@ public class Coins {
     }
 
     public static ItemStack getExchange(ItemStack itemStack, boolean toBigger) {
-        int index = getCoins().indexOf(itemStack) + (toBigger ? 1 : -1);
+        int index = getCoins().indexOf(itemStack);
         if (index < 1 && !toBigger || (index == getCoins().size() - 1) && toBigger) return null;
+        index += (toBigger ? 1 : -1);
         return new ItemStack(getCoins().get(index).getItem(), toBigger ? 1 : 64);
     }
 }
