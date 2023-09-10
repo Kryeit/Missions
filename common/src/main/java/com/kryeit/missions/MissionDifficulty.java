7a5421bfe49dda7b61509b68b8897f89b89dbd28
muriplz <46825658+muriplz@users.noreply.github.com>
2023-09-10 17:24:03 +0200

    changed all color scheme

--- common/src/main/java/com/kryeit/missions/MissionDifficulty.java
+++ common/src/main/java/com/kryeit/missions/MissionDifficulty.java
@@ -4,9 +4,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 
 public enum MissionDifficulty {
-    EASY(0xFFFFFF, new TranslatableComponent("missions.difficulty.easy")),
-    NORMAL(0x0080FF, new TranslatableComponent("missions.difficulty.normal")),
-    HARD(0x800080, new TranslatableComponent("missions.difficulty.hard"));
+    EASY(0x7ac142, new TranslatableComponent("missions.difficulty.easy")),
+    NORMAL(0x66c8c9, new TranslatableComponent("missions.difficulty.normal")),
+    HARD(0xd49250, new TranslatableComponent("missions.difficulty.hard"));
 
     private final int color;
     private final Component description;
