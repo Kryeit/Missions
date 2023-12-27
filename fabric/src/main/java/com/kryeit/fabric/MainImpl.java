e0c025961fef93b8c590a7143823f718309234e3
muriplz <46825658+muriplz@users.noreply.github.com>
2023-12-27 14:37:16 +0100

    Added /missions command
    which opens Missions GUI

--- fabric/src/main/java/com/kryeit/fabric/MainImpl.java
+++ fabric/src/main/java/com/kryeit/fabric/MainImpl.java
@@ -3,6 +3,8 @@ package com.kryeit.fabric;
 import com.kryeit.Main;
 import com.kryeit.MissionHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 import static com.kryeit.Main.REGISTRATE;
@@ -15,10 +17,23 @@ public class MainImpl implements ModInitializer {
         MissionHandler.registerEvents();
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> Main.handlePlayerLogin(handler.getPlayer()));
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager
+                    .literal("missions")
+                    .executes(context -> {
+                        // This is made by ChatScreenMixin.java
+                        //context.getSource().getClient().setScreen(new MissionScreen());
+                        return 1;
+                    }));
+        });
     }
+
+
 
     public static void finalizeRegistrate() {
         REGISTRATE.register();
     }
+
 
 }
