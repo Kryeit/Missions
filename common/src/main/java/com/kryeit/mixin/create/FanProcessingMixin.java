a8fccf0dc4eb1c25a0c46f7c8fdc6da72f3cc29d
muriplz <46825658+muriplz@users.noreply.github.com>
2023-12-28 15:52:59 +0100

    Fixed Create: Dreams and Desired crash

--- common/src/main/java/com/kryeit/mixin/create/FanProcessingMixin.java
+++ common/src/main/java/com/kryeit/mixin/create/FanProcessingMixin.java
@@ -29,20 +29,52 @@ public class FanProcessingMixin {
     private static void onApplyFanRecipe(ItemEntity entity, FanProcessing.Type type, CallbackInfoReturnable<Boolean> cir, List<ItemStack> stacks) {
         Player closestPlayer = MixinUtils.getClosestPlayer(entity.level, entity.blockPosition());
 
-        if (closestPlayer != null) {
-            for (ItemStack stack : stacks) {
-                if (type == FanProcessing.Type.NONE) return;
+        if (closestPlayer == null) return;
 
-                String id = switch (type) {
-                    case SPLASHING -> "splash";
-                    case SMOKING -> "smoke";
-                    case HAUNTING -> "haunt";
-                    case BLASTING -> "blast";
-                    default -> throw new IllegalStateException("Unexpected fan processing type value: " + type);
-                };
-                MissionManager.incrementMission(closestPlayer.getUUID(), id, Registry.ITEM.getKey(stack.getItem()), stack.getCount());
-            }
+        for (ItemStack stack : stacks) {
+            if (type == null || type == FanProcessing.Type.NONE) return;
+
+            String id = switch (type) {
+                case SPLASHING -> "splash";
+                case SMOKING -> "smoke";
+                case HAUNTING -> "haunt";
+                case BLASTING -> "blast";
+                default -> throw new IllegalStateException("Unexpected fan processing type value: " + type);
+            };
+            MissionManager.incrementMission(closestPlayer.getUUID(), id, Registry.ITEM.getKey(stack.getItem()), stack.getCount());
         }
     }
+
+    // TODO: Cannot get the blockPos in this function.
+//    @Inject(
+//            method = "applyProcessing(Lcom/simibubi/create/content/kinetics/belt/transport/TransportedItemStack;Lnet/minecraft/world/level/Level;Lcom/simibubi/create/content/kinetics/fan/FanProcessing$Type;)Lcom/simibubi/create/content/kinetics/belt/behaviour/TransportedItemStackHandlerBehaviour$TransportedResult;",
+//            at = @At(
+//                    value = "TAIL"
+//            )
+//    )
+//    private static void onApplyProcessing(TransportedItemStack transported, Level level, FanProcessing.Type type, CallbackInfoReturnable<TransportedItemStackHandlerBehaviour.TransportedResult> cir) {
+//        List<TransportedItemStack> transportedStacks = cir.getReturnValue().getOutputs();
+//        List<ItemStack> stacks = new ArrayList<>();
+//        for (TransportedItemStack s : transportedStacks) {
+//            stacks.add(s.stack);
+//        }
+//
+//        Player closestPlayer = MixinUtils.getClosestPlayer(level, );
+//
+//        if (closestPlayer != null) {
+//            for (ItemStack stack : stacks) {
+//                if (type == null || type == FanProcessing.Type.NONE) return;
+//
+//                String id = switch (type) {
+//                    case SPLASHING -> "splash";
+//                    case SMOKING -> "smoke";
+//                    case HAUNTING -> "haunt";
+//                    case BLASTING -> "blast";
+//                    default -> throw new IllegalStateException("Unexpected fan processing type value: " + type);
+//                };
+//                MissionManager.incrementMission(closestPlayer.getUUID(), id, Registry.ITEM.getKey(stack.getItem()), stack.getCount());
+//            }
+//        }
+//    }
 }
 
