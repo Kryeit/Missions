9d05b3829f19eb6dee3216936aa3a65bf52a565f
muriplz <46825658+muriplz@users.noreply.github.com>
2023-12-30 01:32:05 +0100

    bump version

--- fabric/src/main/java/com/kryeit/fabric/MainImpl.java
+++ fabric/src/main/java/com/kryeit/fabric/MainImpl.java
@@ -1,11 +1,8 @@
 package com.kryeit.fabric;
 
 import com.kryeit.Main;
-import com.kryeit.MinecraftServerSupplier;
 import com.kryeit.MissionHandler;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 import static com.kryeit.Main.REGISTRATE;
@@ -18,18 +15,6 @@ public class MainImpl implements ModInitializer {
         MissionHandler.registerEvents();
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> Main.handlePlayerLogin(handler.getPlayer()));
-
-        if (MinecraftServerSupplier.getServer() == null) {
-            ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-                dispatcher.register(ClientCommandManager
-                        .literal("missions")
-                        .executes(context -> {
-                            // This is made by ChatScreenMixin.java
-                            //context.getSource().getClient().setScreen(new MissionScreen());
-                            return 1;
-                        }));
-            });
-        }
     }
 
 
