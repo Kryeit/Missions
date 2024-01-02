d62fc5541d3152307a255961d8a1247c7d27e393
muriplz <46825658+muriplz@users.noreply.github.com>
2024-01-02 23:02:49 +0100

    Fixed Exchanger's shaft not updating when placed

--- common/src/main/java/com/kryeit/content/exchanger/MechanicalExchangerRenderer.java
+++ common/src/main/java/com/kryeit/content/exchanger/MechanicalExchangerRenderer.java
@@ -1,19 +1,11 @@
 package com.kryeit.content.exchanger;
 
-import com.jozufozu.flywheel.backend.Backend;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 import com.simibubi.create.foundation.render.CachedBufferer;
 import com.simibubi.create.foundation.render.SuperByteBuffer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.core.Direction;
-
-import static net.minecraft.world.level.block.state.properties.BlockStateProperties.FACING;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class MechanicalExchangerRenderer extends KineticBlockEntityRenderer<MechanicalExchangerBlockEntity> {
     public MechanicalExchangerRenderer(BlockEntityRendererProvider.Context context) {
@@ -21,18 +13,7 @@ public class MechanicalExchangerRenderer extends KineticBlockEntityRenderer<Mech
     }
 
     @Override
-    protected void renderSafe(MechanicalExchangerBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer,
-                              int light, int overlay) {
-        if (Backend.canUseInstancing(be.getLevel())) return;
-        Direction direction = be.getBlockState()
-                .getValue(FACING);
-        VertexConsumer vb = buffer.getBuffer(RenderType.cutoutMipped());
-
-        int lightBehind = LevelRenderer.getLightColor(be.getLevel(), be.getBlockPos().relative(direction.getOpposite()));
-
-        SuperByteBuffer shaftHalf =
-                CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), direction.getOpposite());
-
-        standardKineticRotationTransform(shaftHalf, be, lightBehind).renderInto(ms, vb);
+    protected SuperByteBuffer getRotatedModel(MechanicalExchangerBlockEntity be, BlockState state) {
+        return CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, state);
     }
 }
