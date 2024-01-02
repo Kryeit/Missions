ec3cb5ec249f9c8ea4ec5b247ad41368aa78ed51
muriplz <46825658+muriplz@users.noreply.github.com>
2024-01-02 23:06:46 +0100

    Fixed Exchanger's shaft not updating when placed

--- common/src/main/java/com/kryeit/content/exchanger/MechanicalExchangerBlockEntity.java
+++ common/src/main/java/com/kryeit/content/exchanger/MechanicalExchangerBlockEntity.java
@@ -17,7 +17,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
@@ -179,15 +178,14 @@ public class MechanicalExchangerBlockEntity extends KineticBlockEntity
         Containers.dropContents(this.level, this.worldPosition, this);
     }
 
-    public void tick(Level level, BlockPos pos, BlockState state, MechanicalExchangerBlockEntity blockEntity) {
+    @Override
+    public void tick() {
         updateMode();
         if (hasRecipe()) {
             progress++;
-            setChanged(level, pos, state);
             if (progress > maxProgress) craftItem();
         } else {
             resetProgress();
-            setChanged(level, pos, state);
         }
     }
 
@@ -246,8 +244,8 @@ public class MechanicalExchangerBlockEntity extends KineticBlockEntity
     }
 
     public void updateMode() {
-        if(getSpeed() >= 100) mode = Mode.TO_BIGGER;
-        else if(getSpeed() <= -100) mode = Mode.TO_SMALLER;
+        if (getSpeed() >= 100) mode = Mode.TO_BIGGER;
+        else if (getSpeed() <= -100) mode = Mode.TO_SMALLER;
         else mode = Mode.OFF;
     }
 
