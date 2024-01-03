12506252a1477a2cdcf4fe4f16bd016cfc2c1a2d
muriplz <46825658+muriplz@users.noreply.github.com>
2024-01-03 17:49:53 +0100

    fix command registration

--- fabric/src/main/java/com/kryeit/MainClient.java
+++ fabric/src/main/java/com/kryeit/MainClient.java
@@ -1,13 +1,29 @@
 package com.kryeit;
 
+import com.kryeit.client.screen.MissionScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import org.lwjgl.glfw.GLFW;
+
 
 public class MainClient implements ClientModInitializer {
 
+    public static KeyMapping missionGuiKey;
+
     @Override
     public void onInitializeClient() {
+        missionGuiKey = KeyBindingHelper.registerKeyBinding(new KeyMapping("missions.menu.key", GLFW.GLFW_KEY_H, "missions.key.category"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (missionGuiKey.isDown() && client.screen == null) {
+                client.setScreen(new MissionScreen());
+            }
+        });
+
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(ClientCommandManager
                     .literal("missions")
