14445c621e78cc717986e1397e5ded0bae63524e
Jay Turner <jaynicholasturner@gmail.com>
2024-05-18 10:06:16 +0100

    Load ModStats server side

--- common/src/main/java/com/kryeit/MissionsClient.java
+++ common/src/main/java/com/kryeit/MissionsClient.java
@@ -1,13 +1,11 @@
 package com.kryeit;
 
 import com.kryeit.registry.ModPonders;
-import com.kryeit.registry.ModStats;
 
 public class MissionsClient {
 
     public static void initializeClient() {
         ModPonders.register();
-        ModStats.register();
 
     }
 }
