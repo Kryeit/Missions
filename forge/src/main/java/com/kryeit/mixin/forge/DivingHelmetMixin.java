0caf75537868574e0f4227ecc7d022eade0b7b21
muriplz <46825658+muriplz@users.noreply.github.com>
2025-06-13 20:27:06 +0200

    Fix #61

--- forge/src/main/java/com/kryeit/mixin/forge/DivingHelmetMixin.java
+++ forge/src/main/java/com/kryeit/mixin/forge/DivingHelmetMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.entity.living.LivingBreatheEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,7 +23,7 @@ import static com.simibubi.create.content.equipment.armor.DivingHelmetItem.getWo
 @Mixin(value = DivingHelmetItem.class, remap = false)
 public class DivingHelmetMixin {
     @Inject(method = "breatheUnderwater", at = @At("HEAD"))
-    private static void onDive(LivingEvent.LivingTickEvent event, CallbackInfo ci) {
+    private static void onDive(LivingBreatheEvent event, CallbackInfo ci) {
         if (!(event.getEntity() instanceof ServerPlayer player)) return;
 
         ItemStack helmet = getWornItem(event.getEntity());
@@ -41,7 +42,7 @@ public class DivingHelmetMixin {
             return;
 
         if (lavaDiving) {
-            DivingMission.handleTimeChange(player.getUUID(), 1, new ResourceLocation("minecraft", "lava"));
+            DivingMission.handleTimeChange(player.getUUID(), 1, ResourceLocation.fromNamespaceAndPath("minecraft", "lava"));
         } else {
             BlockState blockState = event.getEntity().level().getBlockState(BlockPos.containing(event.getEntity().getEyePosition()));
 
